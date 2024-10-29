27e6e22458560b15ab6eae01ae307105674fc0c7
SmellyElld <60914@gymnasium.ax>
2024-10-29 22:52:39 +0200

    icons, hourly forecast

--- src/components/ForecastResult.vue
+++ src/components/ForecastResult.vue
@@ -11,36 +11,96 @@ function getText(code) {
     }).description ?? 'Unknown';
     return wcText
 }
+
+function getIcon(code) {
+    let wcIcon = weatherCodes.value.find(itm => {
+        return itm.code == code
+    }).icon_night ?? 'default.svg'
+    return new URL(`../assets/icons/${wcIcon}`, import.meta.url);
+}
+
+function showForecast(date) {
+    let forecastDays = document.getElementsByClassName('forecastDay');
+
+    for (let forecast of forecastDays){
+        if (forecast.id == 'forecastID' + date) {
+            forecast.classList.remove('unselected')
+            forecast.classList.add('selected')
+        } else {
+            forecast.classList.remove('selected')
+            forecast.classList.add('unselected')
+        }
+    }
+}
+
+function getDayNameShort(date, type='short') {
+    // Array of day names
+    const dayNames = [
+        'Söndag',
+        'Måndag',
+        'Tisdag',
+        'Onsdag',
+        'Torsdag',
+        'Fredag',
+        'Lördag'
+    ];
+    
+    // Get the day of the week (0-6)
+    const dayIndex = date.getDay();
+
+    //Return
+    if (type === 'short') {
+        return dayNames[dayIndex].substring(0, 2);
+    }
+    if (type === 'full') {
+        return dayNames[dayIndex];
+    }
+}
 </script>
 
 <template>
-    <ul id="header">
-        <li>Date</li>
-        <li>Temp</li>
-        <li>Precip</li>
-        <li>Wind</li>
-    </ul>
-    <div v-for="day in props.forecast.weather" :key="day" class="forecastNav">
-        <div class="dayCard" :id="'daycardID' + day.date" @click="showForecast(day.date)">
-
+    <section v-if="props.forecast.weather">
+        <div class="forecastNav">
+            <div v-for="day in props.forecast.weather.days" :key="day">
+                <div class="dayCard" :id="'daycardID' + day.date" @click="showForecast(day.date)">
+                    <span>{{ getDayNameShort(new Date(day.date)) }}</span>
+                    <span>{{ getText(day.code) }}</span>
+                    {{ day.temp.average }}{{ props.forecast.weather.units.temp }}
+                </div>
+            </div>
         </div>
-
-        <li>
-            {{ new Date(day.date).getDate() }}.{{ new Date(day.date).getMonth() + 1 }}<br>
-            {{ getText(day.code) }}
-        </li>
-        <li>{{ day.temp.min }}{{ day.temp.unit }} - {{ day.temp.max }}{{ day.temp.unit }}</li>
-        <li>{{ day.precipitation.sum }}{{ day.precipitation.unit }}<br>({{ day.precipitation.probability}}%)</li>
-        <li>{{ Math.round(day.wind.speed) }} ({{ Math.round(day.wind.gusts) }}){{ day.wind.unit }}<br>{{ day.wind.direction }}{{ day.wind.direction_unit }}</li>
-    </div>
-    <div class="forecast"></div>
+        <div class="forecast">
+            <div v-for="day in props.forecast.weather.days" :key="day" class="forecastDay unselected" :id="'forecastID' + day.date">
+                <div>
+                    {{ getDayNameShort(new Date(day.date), 'full') }} {{ (new Date(day.date)).getDate() }}.{{ (new Date(day.date)).getMonth() + 1}}
+                </div>
+                <ul v-for="hour in day.hourly" :key="hour">
+                    <li>{{ hour.time }}</li>
+                    <li><img :src="getIcon(hour.code)" :title="getText(hour.code)"></li>
+                    <li>
+                        {{ hour.wind_direction }}<br>
+                        {{ hour.wind_speed }}<br>
+                        ({{ hour.wind_gusts }})<br>
+                        {{ props.forecast.weather.units.wind_speed }}
+                    </li>
+                    <li>
+                        {{ hour.temp }}{{ props.forecast.weather.units.temp }} <br>
+                        ({{ hour.apparent_temp }}{{ props.forecast.weather.units.temp }})
+                    </li>
+                    <li>
+                        {{ hour.precip }}{{  props.forecast.weather.units.precip }}<br>
+                        {{ hour.precip_prob }}%
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped>
     ul {
         padding:  5px;
         display: grid;
-        grid-template-columns: 25% 25% 20% auto;
         border-radius: 10px;
         align-items: center;
     }
@@ -56,12 +116,22 @@ function getText(code) {
         vertical-align: top;
         font-weight: 350;
     }
-    ul#header {
-        padding:  5px;
-        background-color: rgb(0, 208, 255);
+    .selected {
+        display: flex;
     }
-    ul#header li {
-        font-size: 1.05em;
-        font-weight: 450;
+    .unselected {
+        display: none;
+    }
+
+    .forecastNav {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .dayCard {
+        display: flex;
+        flex-direction: column;
+        background-color: aqua;
+        margin: 5px;
     }
 </style>
--- src/services/forecastService.js
+++ src/services/forecastService.js
@@ -1,7 +1,7 @@
 export function getForecast(location) {
     return new Promise((resolve, reject) => {
         fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${location.position.lat}&longitude=${location.position.long}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&wind_speed_unit=ms&timezone=auto`
+            `https://api.open-meteo.com/v1/forecast?latitude=${location.position.lat}&longitude=${location.position.long}&hourly=temperature_2m,apparent_temperature,precipitation_probability,precipitation,weather_code,wind_speed_10m,wind_gusts_10m,wind_direction_10m&wind_speed_unit=ms&timezone=auto&daily=weather_code`
         ) .then(response => {
             if (response.ok) {
                 return response.json()
@@ -32,50 +32,74 @@ export function getCurrent(location){
 }
 
 /**
- * 
+ * Transforms data from open metio to a more usable format
  * @param {object} data 
  * @param {int} flags 
  * 1:forecast 2:current
  * @returns 
  */
 function transformData(data, flags){
-    let wheaterData = {};
-    wheaterData.position = {lat: data.latitude, long: data.longitude};
-    wheaterData.timezone = {offset: data.utc_offset_seconds, name: data.timezone, short: data.timezone_abbreviation};
+
+    let weatherData = {};
+    weatherData.position = {lat: data.latitude, long: data.longitude};
+    weatherData.timezone = {offset: data.utc_offset_seconds, name: data.timezone, short: data.timezone_abbreviation};
 
     if (flags == 1) {
-        wheaterData.weather = [];
-        for (let i=0; i < data.daily.time.length; i++) {
-            let dayData = {
-                date: data.daily.time[i],
-                code: data.daily.weather_code[i],
-                temp: {
-                    max: data.daily.temperature_2m_max[i],
-                    min: data.daily.temperature_2m_min[i],
-                    unit: data.daily_units.temperature_2m_max
-                },
-                precipitation: {
-                    sum: data.daily.precipitation_sum[i],
-                    probability: data.daily.precipitation_probability_max[i],
-                    unit: data.daily_units.precipitation_sum
-                },
-                wind: {
-                    direction: data.daily.wind_direction_10m_dominant[i],
-                    direction_unit: data.daily_units.wind_direction_10m_dominant,
-                    speed: data.daily.wind_speed_10m_max[i],
-                    gusts: data.daily.wind_gusts_10m_max[i],
-                    unit: data.daily_units.wind_speed_10m_max
-                },
-                sun: {
-                    rise: data.daily.sunrise[i],
-                    set: data.daily.sunset[i],
-                    time_type: data.daily_units.sunrise
+        let days = [];
+        //loop for week
+        for (let currentDay=0; currentDay < data.hourly.time.length / 24; currentDay++) {
+
+            let day = {};
+            day.hourly = {};
+            day.precip_sum = 0;
+            day.temp = {
+                min: data.hourly.apparent_temperature[0],
+                max: data.hourly.apparent_temperature[0],
+                average: data.hourly.apparent_temperature[0]
+            }
+
+            //loop for day
+            for (let i=1; i < 24; i++) {
+                let t = currentDay*24 + i;
+
+                day.hourly[i] = {
+                    time: String(data.hourly.time[t]).split('T')[1],
+                    temp: data.hourly.temperature_2m[t], 
+                    apparent_temp: data.hourly.apparent_temperature[t],
+                    precip: data.hourly.precipitation[t],
+                    precip_prob: data.hourly.precipitation_probability[t],
+                    code: data.hourly.weather_code[t],
+                    wind_speed: data.hourly.wind_speed_10m[t],
+                    wind_gusts: data.hourly.wind_gusts_10m[t],
+                    wind_direction: data.hourly.wind_direction_10m[t],
                 }
-            };
-            wheaterData.weather.push(dayData);
+
+                //Get precip sum and min/max temp of the day
+                day.precip_sum =+ Number(data.hourly.precipitation[t].toFixed(1))
+            }
+
+            day.date = String(data.hourly.time[currentDay*24]).split('T')[0];
+            day.code = data.daily.weather_code[data.daily.time.indexOf(day.date)];
+            day.temp.min = Math.min(... data.hourly.temperature_2m.slice(currentDay*24, currentDay*24 + 24));
+            day.temp.max = Math.max(... data.hourly.temperature_2m.slice(currentDay*24, currentDay*24 + 24));
+            day.temp.average = Number(((data.hourly.temperature_2m.slice(currentDay*24, currentDay*24 + 24).reduceRight((acc, cur) => acc + cur, 0))/24).toFixed(1));
+
+            day.precip_sum
+            days[currentDay] = day;
         }
+        weatherData.weather = {
+            days: days,
+            units: {
+                time: data.hourly_units['time'],
+                temp: data.hourly_units['temperature_2m'],
+                precip: data.hourly_units['precipitation'],
+                wind_speed: data.hourly_units['wind_speed_10m'],
+                wind_direction :data.hourly_units['wind_direction_10m'],
+            }
+        }
+
     } else if (flags == 2) {
-        wheaterData.wheater = {
+        weatherData.weather = {
             time: data.current.time,
             code: data.current.weather_code,
             temp: {
@@ -104,6 +128,5 @@ function transformData(data, flags){
             }
         }
     }
-    return wheaterData
+    return weatherData
 }
-
